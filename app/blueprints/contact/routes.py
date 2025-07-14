8df4c7ce2700bf3cb2965aeb049f9ece9fa7b791
Krishna <Krishnathombare43@gmail.com>
2025-07-14 23:02:30 +0530

    Added Flask-Mail Feature to receive user's questions/queries

--- app/blueprints/contact/routes.py
+++ app/blueprints/contact/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template, request, redirect, url_for, flash
+from flask_mail import Message
+from app import mail
+from dotenv import load_dotenv
+import os
 from . import contact_bp
 from app.forms.contact_form import ContactForm
+
+load_dotenv()
 
 @contact_bp.route("/contactus", methods=["GET", "POST"])
 def contact_us():
@@ -8,7 +14,14 @@ def contact_us():
     previous_url = request.referrer or url_for('home')
 
     if form.validate_on_submit():
-        # Optionally, save to DB or send an email here
+        msg = Message(
+            subject="New Contact Us Message",
+            recipients=[os.getenv('MAIL_USERNAME')],  # where you want to receive the message
+            body=f"Name: {form.name.data}\nEmail: {form.email.data}\nPhone: {form.phone_no.data}\nMessage:\n{form.message.data}"
+        )
+        
+        mail.send(msg)
+        
         flash("Thanks for reaching out! We'll get back to you soon.", "success")
         return redirect(previous_url)
 
