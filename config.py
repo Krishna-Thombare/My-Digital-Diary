516ed92b6248d8d2db587fa6164650387eda7037
Krishna <Krishnathombare43@gmail.com>
2025-07-28 11:13:54 +0530

    Fixed: SQLALCHEMY_DATABASE_URI Error!

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()   # Ensures that environment is loaded before accessing any variables to resolve SQLALCHEMY_DATABASE_URI error
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret')
