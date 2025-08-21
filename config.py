7777abe13f5869193118538a8a36f7b010b8b586
Krishna <Krishnathombare43@gmail.com>
2025-08-21 19:25:16 +0530

    Database Configuration

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import os
 from dotenv import load_dotenv
-from urllib.parse import quote_plus
 
 load_dotenv()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret')
     
     DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = quote_plus(os.getenv('DB_PASSWORD'))
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_HOST = os.getenv('DB_HOST')
     DB_PORT = os.getenv('DB_PORT')
     DB_NAME = os.getenv('DB_NAME')
@@ -16,11 +17,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
-    
+
     SQLALCHEMY_ENGINE_OPTIONS = {
         "connect_args": {
             "ssl": {
-                "ca": os.path.join(os.getcwd(), "certs/ca.pem")
+                "ca": os.path.join(BASE_DIR, "certs/ca.pem")
             }
         }
     }
