94339307ec2e7f2469b4c3d9faa532a0612a3556
Krishna <Krishnathombare43@gmail.com>
2025-08-21 14:20:20 +0530

    Database Config

--- config.py
+++ config.py
@@ -5,5 +5,14 @@ load_dotenv()   # Ensures that environment is loaded before accessing any variab
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    
+    # Database Info
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_NAME = os.getenv('DB_NAME')
+    
+    # Database URI
+    SQLALCHEMY_DATABASE_URI = f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?ssl_mode=REQUIRED"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
