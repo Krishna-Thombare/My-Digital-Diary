58bbd0ddd4c82b197dccccee46f884275c41f783
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-07-12 11:51:54 +0530

    add api route

--- app/blueprints/api_docs/routes.py
+++ app/blueprints/api_docs/routes.py
@@ -2,7 +2,7 @@ from flask import render_template
 from flask_login import login_required
 from . import api_docs_bp
 
-@api_docs_bp.route("/api_docs")
+@api_docs_bp.route("/")
 @login_required
 def api_docs():
     return render_template('api_docs/api_docs.html')
